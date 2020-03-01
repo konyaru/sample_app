547e130a0495c19f04140ef9ca4f3568958792c4
orira4666 <orira4666@gmail.com>
2020-03-01 07:45:28 +0000

    fix configfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 	config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 	host = 'floating-stream-88485.herokuapp.com'
-	ction_mailer.default_url_options = { host: host }
+	config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',

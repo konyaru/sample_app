254ccd241db666e2f1bd5628c1318428addd370b
orira4666 <orira4666@gmail.com>
2020-04-11 16:08:14 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,22 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 		@user.microposts.paginate(page: 1).each do |micropost|
 			assert_match micropost.content, response.body
 		end
+		assert_match @user.active_relationships.count.to_s, response.body
+		assert_match @user.passive_relationships.count.to_s, response.body
+	end
+
+	test "layout links wher logged in user" do
+		log_in_as(@user)
+		get root_path
+		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
 	end
 end
